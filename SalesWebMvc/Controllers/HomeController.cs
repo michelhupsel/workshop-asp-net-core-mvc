8a9d78e098277dd28f3403ebf9bfd472ea4ee821
michelhupsel <michelhupsel@gmail.com>
2019-04-25 11:15:05 -0300

    Primeiro controller e pagina Razor de teste

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["Nome"] = "Michel";
             return View();
         }
 
